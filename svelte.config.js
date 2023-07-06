132d63a72e061c66f2f43d46f4737f1f98076a22
mere-patrie <angus.paillaugue40@gmail.com>
2023-07-06 17:29:27 +0200

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js'; 
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,7 @@ const config = {
 		mdsvex(mdsvexConfig),
     ],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({})
 	},
 	vitePlugin: {
 		inspector: true,
